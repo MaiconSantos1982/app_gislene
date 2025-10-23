d3fb3666c8960f71566e05834904339c6d72136a
MaiconSantos1982 <115635790+MaiconSantos1982@users.noreply.github.com>
2025-10-23 15:06:10 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Configurações do Supabase
+const SUPABASE_URL = 'SUA_URL_AQUI';
+const SUPABASE_ANON_KEY = 'SUA_CHAVE_PUBLICA_AQUI';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
